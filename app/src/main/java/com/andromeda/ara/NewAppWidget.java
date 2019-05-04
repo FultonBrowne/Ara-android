d142462ebe40a29e7ccb11da0c2c55b1f1c93d39
Michael Browne <michaelbrowne@Browne-home-printer.local>
2019-05-04 15:40:23 -0700

    added new layout stuff NOT READY TO TEST

---
+++ app/src/main/java/com/andromeda/ara/NewAppWidget.java
@@ -0,0 +1,28 @@
+package com.andromeda.ara;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+

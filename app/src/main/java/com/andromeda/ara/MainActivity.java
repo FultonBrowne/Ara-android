5a51b102c7535ac7810f519b60c116ebba666c04
FultonB <gprlabs@outlook.com>
2019-05-22 21:56:23 +0000

    this is getting old

--- app/src/main/java/com/andromeda/ara/MainActivity.java
+++ app/src/main/java/com/andromeda/ara/MainActivity.java
@@ -65,14 +65,14 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    private List<ContactInfo> createList(int size) {
+    private List<info> createList(int size) {
 
-        List<ContactInfo> result = new ArrayList<ContactInfo>();
+        List<info> result = new ArrayList<info>();
         for (int i=1; i <= size; i++) {
-            ContactInfo ci = new ContactInfo();
-            ci.name = ContactInfo.NAME_PREFIX + i;
-            ci.surname = ContactInfo.SURNAME_PREFIX + i;
-            ci.email = ContactInfo.EMAIL_PREFIX + i + "@test.com";
+            info ci = new info();
+            ci.name = info.NAME_PREFIX + i;
+            ci.surname = info.SURNAME_PREFIX + i;
+            ci.email = info.EMAIL_PREFIX + i + "@test.com";
 
             result.add(ci);
 

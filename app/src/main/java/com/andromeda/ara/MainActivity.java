040ff6f22583c7bce9962cf44340955e38684dd9
FultonB <gprlabs@outlook.com>
2019-05-22 22:00:59 +0000

    maybe this time

--- app/src/main/java/com/andromeda/ara/MainActivity.java
+++ app/src/main/java/com/andromeda/ara/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 

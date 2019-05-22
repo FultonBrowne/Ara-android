7e17e4219bcb5aaefb5ed5be1c89768acac74c6f
FultonB <gprlabs@outlook.com>
2019-05-22 21:47:32 +0000

    try try try again

--- app/src/main/java/com/andromeda/ara/MainActivity.java
+++ app/src/main/java/com/andromeda/ara/MainActivity.java
@@ -70,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }

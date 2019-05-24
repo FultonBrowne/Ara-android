a999815e463f3d29a56ec4eefe5a9d2390310edd
andromeda-software-mac2012 <gprlabs@outlook.com>
2019-05-24 14:13:50 -0700

    added voice stuff DOES NOT WORK YET

--- app/src/main/java/com/andromeda/ara/voiceActivity.java
+++ app/src/main/java/com/andromeda/ara/voiceActivity.java
@@ -1,6 +1,6 @@
 package com.andromeda.ara;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class voiceActivity extends AppCompatActivity {

f5c1ea82649b0c51fe38ea435c493dbade249af3
andromeda-software-mac2012 <gprlabs@outlook.com>
2019-05-23 17:13:20 -0700

    added voice stuff DOES NOT WORK YET

---
+++ app/src/main/java/com/andromeda/ara/voiceActivity.java
@@ -0,0 +1,13 @@
+package com.andromeda.ara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class voiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice);
+    }
+}

ab314a3b463a8021ba5d940a554e7c5e17efddf3
andromeda-software-mac2012 <gprlabs@outlook.com>
2019-06-01 12:36:33 -0700

    changed some stuff

---
+++ app/src/main/java/com/andromeda/ara/setup.java
@@ -0,0 +1,14 @@
+package com.andromeda.ara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+}

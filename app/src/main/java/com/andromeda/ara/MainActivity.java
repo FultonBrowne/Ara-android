f45c3b7ef4c36478e495051b505355c5d2340e3a
Robotics <R@robot.com>
2019-06-03 20:07:02 -0700

    added some listviews

--- app/src/main/java/com/andromeda/ara/MainActivity.java
+++ app/src/main/java/com/andromeda/ara/MainActivity.java
@@ -1,5 +1,6 @@
 package com.andromeda.ara;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;

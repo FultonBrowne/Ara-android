2841243cf09e6482d8c60db6f5200c45a7593118
andromeda-software-mac2012 <gprlabs@outlook.com>
2019-06-08 17:18:38 -0700

    added some voice stuff

--- app/src/main/java/com/andromeda/ara/HotWord.java
+++ app/src/main/java/com/andromeda/ara/HotWord.java
@@ -1,14 +1,89 @@
 package com.andromeda.ara;
 
 
+import android.content.Intent;
 import android.service.voice.AlwaysOnHotwordDetector;
+import android.service.voice.VoiceInteractionService;
+import android.util.Log;
+import android.app.IntentService;
+
+
+
+
 
 import java.util.Locale;
 
-abstract class HotWord {
-    public abstract AlwaysOnHotwordDetector createAlwaysOnHotwordDetector (String keyphrase,
-                                                                        Locale locale,
-                                                                        AlwaysOnHotwordDetector.Callback callback);
+
+
+public class HotWord extends VoiceInteractionService {
+    static final String TAG = "MainInteractionService";
+
+            private final AlwaysOnHotwordDetector.Callback mHotwordCallback = new AlwaysOnHotwordDetector.Callback() {
+        @Override
+        public void onAvailabilityChanged(int status) {
+                        Log.i(TAG, "onAvailabilityChanged(" + status + ")");
+                        hotwordAvailabilityChangeHelper(status);
+                    }
+
+                @Override
+        public void onDetected(AlwaysOnHotwordDetector.EventPayload eventPayload) {
+                        Log.i(TAG, "onDetected");
+                    ;
+
+                }
+
+                @Override
+        public void onError() {
+                        Log.i(TAG, "onError");
+                    }
+
+                @Override
+        public void onRecognitionPaused() {
+                        Log.i(TAG, "onRecognitionPaused");
+                   }
+
+                @Override
+        public void onRecognitionResumed() {
+                        Log.i(TAG, "onRecognitionResumed");
+                   }
+    };
+
+            private AlwaysOnHotwordDetector mHotwordDetector;
+    @Override
+    public void onReady(){
+        super.onReady();
+
+
+               mHotwordDetector = createAlwaysOnHotwordDetector(
+                               "Hey Ara", Locale.forLanguageTag("en-US"), mHotwordCallback);
+           }
+
+    private void hotwordAvailabilityChangeHelper(int availability) {
+        Log.i(TAG, "Hotword availability = " + availability);
+        switch (availability) {
+            case AlwaysOnHotwordDetector.STATE_HARDWARE_UNAVAILABLE:
+                Log.i(TAG, "STATE_HARDWARE_UNAVAILABLE");
+                break;
+            case AlwaysOnHotwordDetector.STATE_KEYPHRASE_UNSUPPORTED:
+                Log.i(TAG, "STATE_KEYPHRASE_UNSUPPORTED");
+                break;
+            case AlwaysOnHotwordDetector.STATE_KEYPHRASE_UNENROLLED:
+                Log.i(TAG, "STATE_KEYPHRASE_UNENROLLED");
+                Intent enroll = mHotwordDetector.createEnrollIntent();
+                Log.i(TAG, "Need to enroll with " + enroll);
+                break;
+            case AlwaysOnHotwordDetector.STATE_KEYPHRASE_ENROLLED:
+                Log.i(TAG, "STATE_KEYPHRASE_ENROLLED - starting recognition");
+                if (mHotwordDetector.startRecognition(
+                        0)) {
+                    Log.i(TAG, "startRecognition succeeded");
+                } else {
+                    Log.i(TAG, "startRecognition failed");
+                }
+                break;
+        }
+    }
+
 
 
 

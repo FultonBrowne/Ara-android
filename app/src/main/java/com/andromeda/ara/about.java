686db685b810f284aff5ce09ab3a082d3f3b31c5
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-07-23 11:08:04 -0700

    Revert "Revert "Revert "added kotlin stuff!"""
    This reverts commit f2c1531a7451a2ae896436072355d2a1858ea472.

---
+++ app/src/main/java/com/andromeda/ara/about.java
@@ -0,0 +1,31 @@
+package com.andromeda.ara;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}

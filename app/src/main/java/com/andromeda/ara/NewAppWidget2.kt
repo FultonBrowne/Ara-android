3c5d64ec998b8bb786ecf15fbbfb288c87725a27
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-07-23 15:21:02 -0700

    kotlin3

---
+++ app/src/main/java/com/andromeda/ara/NewAppWidget2.kt
@@ -0,0 +1,51 @@
+package com.andromeda.ara
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in [NewAppWidget2ConfigureActivity]
+ */
+class NewAppWidget2 : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (appWidgetId in appWidgetIds) {
+            NewAppWidget2ConfigureActivity.deleteTitlePref(context, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    companion object {
+
+        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager,
+                                     appWidgetId: Int) {
+
+            val widgetText = NewAppWidget2ConfigureActivity.loadTitlePref(context, appWidgetId)
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.new_app_widget2)
+            views.setTextViewText(R.id.appwidget_text, widgetText)
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
---
+++ app/src/main/java/com/andromeda/ara/setup.kt
@@ -0,0 +1,13 @@
+package com.andromeda.ara
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class setup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
+    }
+}

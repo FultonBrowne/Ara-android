9d0fc1d3465dffa22e636e643402467ffb8db4aa
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-07-23 12:56:01 -0700

    kotlin stufff that works!

---
+++ app/src/main/java/com/andromeda/ara/search.kt
@@ -0,0 +1,12 @@
+package com.andromeda.ara
+
+class search {
+    fun main(mainval: String, mode:Int): RssFeedModel{
+        val main1: RssFeedModel
+        main1 = RssFeedModel("","","","")
+        if (mode == 1){
+
+        }
+        return main1
+    }
+}

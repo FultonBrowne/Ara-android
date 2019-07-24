fb7b8196edcd1bec7ae357b2cf827a7fc35698a3
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-07-24 12:51:04 -0700

    added some more stuff

--- app/src/main/java/com/andromeda/ara/GetFood.kt
+++ app/src/main/java/com/andromeda/ara/GetFood.kt
@@ -1,8 +1,14 @@
 package com.andromeda.ara
 
+import java.net.URL
+
 class GetFood {
     fun nearMe(city: String) : RssFeedModel{
         var main :RssFeedModel = RssFeedModel("", "", "","")
+        // TODO work on this
+        khttp.get(
+                url = "http://httpbin.org/get",
+                params = mapOf("key1" to "value1", "keyn" to "valuen"))
         return main
 
     }

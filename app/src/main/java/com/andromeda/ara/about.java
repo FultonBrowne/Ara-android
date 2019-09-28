0261006f5e2f3acf4536958056d96a8eebf25111
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-09-28 15:57:54 -0700

    Update about.java

--- app/src/main/java/com/andromeda/ara/about.java
+++ app/src/main/java/com/andromeda/ara/about.java
@@ -35,13 +35,8 @@ public class about extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }

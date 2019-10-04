ac06240836bd6f7916b6701bf22bbbc748f129a6
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-10-04 15:27:20 -0700

    made some changes to the main voice activity

--- app/src/main/java/com/andromeda/ara/voice/VoiceMain.java
+++ app/src/main/java/com/andromeda/ara/voice/VoiceMain.java
@@ -16,24 +16,50 @@
 
 package com.andromeda.ara.voice;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.andromeda.ara.R;
+import com.andromeda.ara.search.Search;
+import com.andromeda.ara.util.RssFeedModel;
+
+import java.util.ArrayList;
 
 public class VoiceMain extends AppCompatActivity {
+    private static final int REQUEST_RECORD_AUDIO = 13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice_main);
+        Context ctx = this;
+        requestMicrophonePermission();
+
+        String phrase = new run().run1(ctx, this);
+        Toast.makeText(ctx, phrase, Toast.LENGTH_LONG).show();
+        //TODO get lat and log
+        ArrayList<RssFeedModel> toFeed = new Search().main(phrase, "0", "0");
+
+
 
     }
 
     public void back(View view) {
         onBackPressed();
+
+    }
+    private void requestMicrophonePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(
+                    new String[]{android.Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
+
+        }
+
 
     }
 }

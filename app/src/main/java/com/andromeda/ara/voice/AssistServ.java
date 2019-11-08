541083de94e54d70b26aabd5c76d595e38605909
fulton <gprlabs@outlook.com>
2019-11-08 14:38:31 -0800

    messing around with hotWord detectors

--- app/src/main/java/com/andromeda/ara/voice/AssistServ.java
+++ app/src/main/java/com/andromeda/ara/voice/AssistServ.java
@@ -16,16 +16,60 @@
 
 package com.andromeda.ara.voice;
 
+import android.content.Intent;
+import android.service.voice.AlwaysOnHotwordDetector;
 import android.service.voice.VoiceInteractionService;
+import android.service.voice.VoiceInteractionSession;
+import android.service.voice.VoiceInteractionSessionService;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.andromeda.ara.R;
 
+import java.util.Locale;
+
 //@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class AssistServ extends VoiceInteractionService {
+    private static final String TAG = "v";
+    public AlwaysOnHotwordDetector mHotwordDetector;
+    private AlwaysOnHotwordDetector.Callback mHotwordCallback = new AlwaysOnHotwordDetector.Callback() {
+
+
+        @Override
+        public void onAvailabilityChanged(int status) {
+            Log.i(TAG, "onAvailabilityChanged($status)");
+            hotwordAvailabilityChangeHelper(status);
+
+        }
+
+        @Override
+        public void onDetected(@NonNull AlwaysOnHotwordDetector.EventPayload eventPayload) {
+
+        }
+
+        @Override
+        public void onError() {
+
+        }
+
+        @Override
+        public void onRecognitionPaused() {
+
+        }
+
+        @Override
+        public void onRecognitionResumed() {
+
+        }
+
+
+    };
     @Override
     public void onReady() {
         super.onReady();
+         mHotwordDetector = createAlwaysOnHotwordDetector(
+                "Hey Ara", Locale.forLanguageTag("en-US"), mHotwordCallback);
 
 
         Log.v(getString(R.string.tag_AssistantService), getString(R.string.msg_onReady));
@@ -38,4 +82,40 @@ public class AssistServ extends VoiceInteractionService {
         Log.v(getString(R.string.tag_AssistantService), getString(R.string.msg_onCreate));
     }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+        private void hotwordAvailabilityChangeHelper(Integer availability) {
+            Log.i(TAG, "Hotword availability = $availability");
+            switch (availability) {
+                case AlwaysOnHotwordDetector.STATE_HARDWARE_UNAVAILABLE : Log.i(TAG, "STATE_HARDWARE_UNAVAILABLE");
+                case AlwaysOnHotwordDetector.STATE_KEYPHRASE_UNSUPPORTED : Log.i(TAG, "STATE_KEYPHRASE_UNSUPPORTED");
+                case AlwaysOnHotwordDetector.STATE_KEYPHRASE_UNENROLLED : {
+                    Log.i(TAG, "STATE_KEYPHRASE_UNENROLLED");
+                    try {
+                        Intent enroll = mHotwordDetector.createEnrollIntent();
+                        Log.i(TAG, "Need to enroll with " + enroll);
+                    }
+                    catch (Exception e){}
+
+                }
+                case AlwaysOnHotwordDetector.STATE_KEYPHRASE_ENROLLED :{
+                    Log.i(TAG, "STATE_KEYPHRASE_ENROLLED - starting recognition");
+                    try{
+                    if (mHotwordDetector.startRecognition(
+                            0)) {
+                        Log.i(TAG, "startRecognition succeeded");
+                    } else {
+                        Log.i(TAG, "startRecognition failed");
+                    }
+                }
+                    catch (Exception e){}
+
+                }
+                break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + availability);
+            }
+        }
 }

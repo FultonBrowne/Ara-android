d627798c763599a1655299febd93a5292407cd86
fulton <gprlabs@outlook.com>
2020-01-16 11:00:17 -0800

    working on image over search

--- app/src/main/java/com/andromeda/ara/util/OutputModel.java
+++ app/src/main/java/com/andromeda/ara/util/OutputModel.java
@@ -23,13 +23,15 @@ public class OutputModel {
     public String description;
     public String OutputTxt;
     public String exes;
+    public String image;
 
-    OutputModel(String title, String description, String link, String OutputTxt, String exes) {
+    OutputModel(String title, String description, String link, String image, String OutputTxt, String exes) {
         this.title = title;
         this.link = link;
         this.description = description;
         this.OutputTxt = OutputTxt;
         this.exes = exes;
+        this.image = image;
 
     }
 }

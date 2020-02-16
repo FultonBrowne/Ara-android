e665e9c19955c527ab036952982a393d43212040
fulton <gprlabs@outlook.com>
2020-02-16 08:23:03 -0800

    bug fix

--- app/src/main/java/com/andromeda/ara/activitys/MainActivity.java
+++ app/src/main/java/com/andromeda/ara/activitys/MainActivity.java
@@ -238,7 +238,14 @@ public class MainActivity extends AppCompatActivity implements SearchFunctions {
         recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
-                new CardOnClick().mainFun(mode, rssFeedModel1.get(position).link, act, getApplicationContext(), MainActivity.this);
+                try {
+
+
+                    new CardOnClick().mainFun(mode, rssFeedModel1.get(position).link, act, getApplicationContext(), MainActivity.this);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
 
             }
 

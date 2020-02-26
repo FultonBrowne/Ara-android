bd42658017fa965a3f803e796a2989762e304749
fulton <gprlabs@outlook.com>
2020-02-25 17:57:22 -0800

    working on TESTS

---
+++ app/src/test/java/com/andromeda/ara/SearchTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020. Fulton Browne
+ *  This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.andromeda.ara
+
+import android.app.Activity
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.platform.app.InstrumentationRegistry
+import com.andromeda.ara.activitys.MainActivity
+import com.andromeda.ara.search.Search
+import com.andromeda.ara.skills.SearchFunctions
+import com.andromeda.ara.voice.TTS
+import org.junit.Test
+
+
+class SearchTest :SearchFunctions{
+    @Test
+    fun test(){
+        var instrumentationContext: Context =  InstrumentationRegistry.getInstrumentation().context
+        var act:Activity = MainActivity()
+        Search().main("test", instrumentationContext, act,this, RecyclerView(instrumentationContext), TTS(), arrayListOf())
+    }
+
+    override fun callBack(m: String, link: String) {
+
+    }
+
+    override fun callForString(m: String): String {
+        return ""
+    }
+
+}

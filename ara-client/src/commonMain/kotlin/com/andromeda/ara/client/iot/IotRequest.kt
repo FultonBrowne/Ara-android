319849a142f6fc4db9b0dc45e431a882651f06fb
fulton <gprlabs@outlook.com>
2020-04-08 15:59:53 -0700

    iot stuff

---
+++ ara-client/src/commonMain/kotlin/com/andromeda/ara/client/iot/IotRequest.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020. Fulton Browne
+ *  This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.andromeda.ara.client.iot
+
+import com.andromeda.ara.client.util.ReadURL
+import io.ktor.client.HttpClient
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.http.HttpMethod
+import io.ktor.http.URLBuilder
+
+object IotRequest {
+    fun getRequest(command:String){
+        val client = HttpClient()
+        val request = HttpRequestBuilder()
+        request.method = HttpMethod("GET")
+
+    }
+}

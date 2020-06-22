6b5c9d0e658b0cb09518d2f903be1e535a9d2aaf
Fulton Browne arch laptop <gprlabs@outlook.com>
2020-06-22 13:37:16 +0000

    feed json stuff done

--- ara-client/src/commonMain/kotlin/com/andromeda/ara/client/util/JsonParse.kt
+++ ara-client/src/commonMain/kotlin/com/andromeda/ara/client/util/JsonParse.kt
@@ -38,13 +38,13 @@ class JsonParse {
         println(array)
                 return array
     }
-    fun feed(text:String){
+    fun feed(text:String):Feed{
 	    val obj  =  Json.parseJson(text).jsonObject
-	    val action  = obj.getArray("action")
+	    val action  = obj.getArrayOrNull("action")
 	    val mainFeed = obj.getArray("feed")
 	    val actionsArray = arrayListOf<SkillsModel>()
 	    val feedArray = arrayListOf<FeedModel>()
-	    action.forEach{
+	    action?.forEach{
 		    val jo = it.jsonObject
 		    actionsArray.add(SkillsModel(jo.get("action")!!.content,jo.get("arg1")!!.content, jo.get("arg2")!!.content))
 
@@ -53,6 +53,7 @@ class JsonParse {
 		    val jo = it.jsonObject
 		    feedArray.add(FeedModel(jo.get("title")!!.content, jo.get("description")!!.content, jo.get("link")?.contentOrNull, jo.get("image")?.contentOrNull, jo.get("longText")!!.primitive.boolean, jo.get("color")?.primitive?.intOrNull))
 	    }
+	    return Feed(obj.get("type")!!.content, actionsArray, obj.get("voice")?.contentOrNull, feedArray)
     }
     fun reminder(text:String): RemindersModel {
         val array = arrayListOf<RemindersModel>()
